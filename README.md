e37a87f058c4a3b4a63999c7ccf031f6c75601fa
Nicholas McCollum <nimccoll@microsoft.com>
2023-11-13 12:02:47 -0500

    Initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CloudServicesExtendedSupportRestart
 Sample PowerShell scripts to restart Cloud Services Extended Support Role Instances
+
+# RestartRole.ps1
+This script will allow you to restart all instances of a specified role or all role instances for an entire Cloud Services Extended Support service. Supplying a role name will restart all instances for that role. Supplying a wild card (*) for the role name will restart all role instances for the cloud service. The script has been designed to run interactively or via an Azure Automation run book.
+
+## Inputs
+- subscriptionId - Azure subscription ID
+- resourceGroupName - Resource Group Name of the resource group that contains the cloud service
+- cloudServiceName - Cloud Services Extended Support name
+- roleName - The name of the role to restart. All role instances for this role name will be restarted. To restart all role instances for the entire cloud service pass in a wildcard (*).
+- interactive - A switch parameter that when specified via -interactive will run the script in interactive mode prompting for your Azure credentials
+
+## Prerequisites
+- If you plan to run the script via an Azure Automation run book, create a system assigned managed identity for your Azure Automation account following the steps outlined [here](https://learn.microsoft.com/en-us/azure/automation/enable-managed-identity-for-automation#enable-using-the-azure-portal). Assign the system assigned managed identity the Contributor role on the Cloud Services Extended Support service.
+- If you want to run the script interactively, add the -interactive switch.
---
+++ RestartRole.ps1
@@ -0,0 +1,63 @@
+﻿#===============================================================================
+# Microsoft FastTrack for Azure
+# Restart one or all Cloud Services Extended Support Roles
+#===============================================================================
+# Copyright © Microsoft Corporation.  All rights reserved.
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+# OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#===============================================================================
+param(
+    [Parameter(Mandatory)]$subscriptionId,
+    [Parameter(Mandatory)]$resourceGroupName,
+    [Parameter(Mandatory)]$cloudServiceName,
+    [Parameter(Mandatory)]$roleName,
+    [switch]$interactive
+)
+
+if ($interactive -eq $true) {
+    # Login to Azure
+    Connect-AzAccount
+
+    # Set subscription context
+    Set-AzContext -Subscription $subscriptionId
+}
+else {
+    # Ensures you do not inherit an AzContext in your runbook
+    Disable-AzContextAutosave -Scope Process | Out-Null
+
+    # Connect using a Managed Service Identity
+    try {
+        $AzureContext = (Connect-AzAccount -Identity).context
+    }
+    catch {
+        Write-Output "There is no system-assigned user identity. Aborting."; 
+        exit
+    }
+
+    # Set and store context
+    $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+}
+
+# If wildcard is passed in for the role name, restart all role instances
+if ($roleName -eq "*") {
+    Write-Output "Restarting all role instances for cloud service $cloudServiceName..."
+    Restart-AzCloudService -ResourceGroupName $resourceGroupName -CloudServiceName $cloudServiceName -RoleInstance "*"
+    Write-Output "Role instances restarted successfully"
+}
+else {
+    # Only restart role instances that match the provided role name
+    $roleInstanceNames = @()
+    $roleInstances = Get-AzCloudServiceRoleInstance -ResourceGroupName $resourceGroupName -CloudServiceName $cloudServiceName
+    $roleInstances | ForEach-Object {
+        if ($_.Name.StartsWith($roleName)) {
+            $roleInstanceNames += $_.Name
+        }
+    }
+    if ($roleInstanceNames.Length -gt 0) {
+        Write-Output "Restarting the following role instances for cloud service ${cloudServiceName}: $roleInstanceNames..."
+        Restart-AzCloudService -ResourceGroupName $resourceGroupName -CloudServiceName $cloudServiceName -RoleInstance $roleInstanceNames
+        Write-Output "Role instances restarted successfully"
+    }
+}
